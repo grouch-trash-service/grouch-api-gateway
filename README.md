55db0d9d32d430583c5e7e5b959a896be54b25ab
matthew-js-porter <matthew.js.porter@gmail.com>
2020-05-28 01:07:19 -0400

    adding local swagger-ui formation.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Grouch api gateway
+
+## Swagger-ui
+```bash
+docker run -p 8080:8080 -e SWAGGER_JSON=/schema/swagger.yaml -v `pwd`/src/main/resources:/schema swaggerapi/swagger-ui
+```
+
--- lambda/index.py
+++ lambda/index.py
@@ -2,6 +2,7 @@ import json
 def handler(event, context):
     return {
         "statusCode": 200,
+        "headers": {'Access-Control-Allow-Origin': '*'},
         "body": json.dumps({
             "message": "Hello. Trash pickup will be on Tuesday... Now SCRAM!",
         }),
